ebebed2c799adca579abadf054477f5b74236c84
Les Aker <me@lesaker.org>
2014-07-12 17:36:02 -0400

    finish asset specs

--- spec/targit/asset_spec.rb
+++ spec/targit/asset_spec.rb
@@ -35,9 +35,43 @@ describe Targit do
       expect(io_asset.name).to eql 'alpha'
     end
 
+    it 'does not create a release by default' do
+      VCR.use_cassette('asset_without_release') do
+        expect do
+          Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'more_testing',
+            authfile: 'spec/.creds'
+          )
+        end.to raise_error RuntimeError, /No release found/
+      end
+    end
+
+    it 'can create a new release' do
+      VCR.use_cassette('asset_autocreate_release') do
+        expect do
+          Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'more_testing',
+            authfile: 'spec/.creds'
+          )
+        end.to raise_error RuntimeError, /No release found/
+        asset = Targit.new(
+          'spec/examples/beta',
+          'akerl/targit',
+          'more_testing',
+          authfile: 'spec/.creds',
+          create: true
+        )
+        expect(asset.url).to eql 'https://github.com/akerl/targit/releases/download/more_testing/beta'
+      end
+    end
+
     describe '#upload!' do
       it 'uploads a release asset' do
-        VCR.use_cassette('create_new_release') do
+        VCR.use_cassette('create_new_asset') do
           asset = Targit.new(
             'spec/examples/beta',
             'akerl/targit',
@@ -47,6 +81,122 @@ describe Targit do
           expect { asset.url }.to raise_error RuntimeError, /URL not found/
           asset.upload!
           expect(asset.url).to eql 'https://github.com/akerl/targit/releases/download/testing/beta'
+        end
+      end
+      it 'will fail to replace an existing asset by default' do
+        VCR.use_cassette('block_existing_asset') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds',
+          )
+          expect { asset.upload! }.to raise_error RuntimeError, /already exists/
+        end
+      end
+      it 'can replace an existing asset' do
+        VCR.use_cassette('replace_existing_asset') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds',
+            force: true
+          )
+          expect(asset.url).to eql 'https://github.com/akerl/targit/releases/download/testing/beta'
+          asset.upload!
+          expect(asset.url).to eql 'https://github.com/akerl/targit/releases/download/testing/beta'
+        end
+      end
+    end
+
+    describe '#already_exists?' do
+      it 'returns true if the asset is on GitHub' do
+        VCR.use_cassette('asset_existence_check_pass') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.already_exists?).to be_truthy
+        end
+      end
+      it 'returns false if the asset is not on GitHub' do
+        VCR.use_cassette('asset_existence_check_fail') do
+          asset = Targit.new(
+            'spec/examples/charlie',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.already_exists?).to be_falsey
+        end
+      end
+    end
+
+    describe '#delete!' do
+      it 'removes the asset from GitHub' do
+        VCR.use_cassette('delete_asset') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.already_exists?).to be_truthy
+          asset.delete!
+          expect(asset.already_exists?).to be_falsey
+        end
+      end
+    end
+
+    describe '#github_data' do
+      it 'returns the GitHub metadata for this asset' do
+        VCR.use_cassette('asset_metadata_present') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.github_data[:tag_name]).to eql 'testing'
+        end
+      end
+      it 'returns nil if the asset is not on GitHub' do
+        VCR.use_cassette('asset_metadata_absent') do
+          asset = Targit.new(
+            'spec/examples/charlie',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.github_data).to be_nil
+        end
+      end
+    end
+
+    describe '#url' do
+      it 'returns the download URL if the asset is on GitHub' do
+        VCR.use_cassette('asset_url_present') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect(asset.url).to eql 'https://github.com/akerl/targit/releases/download/testing/beta'
+        end
+      end
+      it 'raises if the asset is not on GitHub' do
+        VCR.use_cassette('asset_url_absent') do
+          asset = Targit.new(
+            'spec/examples/beta',
+            'akerl/targit',
+            'testing',
+            authfile: 'spec/.creds'
+          )
+          expect { asset.url }.to raise_error RuntimeError, /URL not found/
         end
       end
     end

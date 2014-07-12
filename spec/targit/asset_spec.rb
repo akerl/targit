a82c5fe3fd151c14b73154f857612bb225407053
Les Aker <me@lesaker.org>
2014-07-12 15:59:33 -0400

    initial asset tests

--- spec/targit/asset_spec.rb
+++ spec/targit/asset_spec.rb
@@ -2,5 +2,36 @@ require 'spec_helper'
 
 describe Targit do
   describe Targit::Asset do
+    let(:asset) do
+      VCR.use_cassette('current_releases') do
+        Targit.new(
+          'spec/examples/alpha',
+          'akerl/targit',
+          'testing',
+          authfile: 'spec/.creds'
+        )
+      end
+    end
+    let(:io_asset) do
+      VCR.use_cassette('current_releases') do
+        fh = File.open('spec/examples/alpha')
+        Targit.new(
+          fh,
+          'akerl/targit',
+          'testing',
+          authfile: 'spec/.creds'
+        )
+      end
+    end
+    it 'exposes the release object' do
+      expect(asset.release).to be_an_instance_of Targit::Release
+    end
+    it 'exposes the asset attribute' do
+      expect(asset.asset).to eql 'spec/examples/alpha'
+    end
+    it 'expostes the asset name' do
+      expect(asset.name).to eql 'alpha'
+      expect(io_asset.name).to eql 'alpha'
+    end
   end
 end

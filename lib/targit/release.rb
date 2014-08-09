e0fda758b1505fbd481b1a35a933ce4e522fb958
Les Aker <me@lesaker.org>
2014-08-08 20:09:25 -0400

    nil or a string

--- lib/targit/release.rb
+++ lib/targit/release.rb
@@ -12,7 +12,7 @@ module Targit
       @client = @options[:client]
       @create_options = _create_options
       @data = find
-      create if @data.nil? && @options[:create]
+      create if @data.nil? && @options.include?(:create)
       fail('No release found') if @data.nil?
     end
 
@@ -31,7 +31,7 @@ module Targit
 
     def _create_options
       opts = {}
-      opts[:name] = @options[:create] if @options[:create].to_s.size
+      opts[:name] = @options[:create] if @options[:create].is_a? String
       [:prerelease, :target_commitish].each_with_object(opts) do |option, hash|
         hash[option] = @options[option] if @options[option]
       end
